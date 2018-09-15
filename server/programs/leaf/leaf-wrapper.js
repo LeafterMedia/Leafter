9a04ae8d882e72ef4f651fb6c064d0a674b0695d
Leafter <42569062+Leafter@users.noreply.github.com>
2018-09-15 15:53:52 +0200

    Update leaf-wrapper.js

--- server/programs/leaf/leaf-wrapper.js
+++ server/programs/leaf/leaf-wrapper.js
@@ -1,16 +1,16 @@
-const leaf 			= require("steem");
+const leaf 		= require("steem");
 const config 		= require('./../../../config.json');
 
-var owner 			= config.leafter.owner;
+var owner 		= config.leafter.owner;
 var owner_wif 		= config.leafter.keys.owner.private;
-var fee				= config.accountCreationParams.fee;
+var fee			= config.accountCreationParams.fee;
 var delegation 		= config.accountCreationParams.delegation;
 var jsonMetadata 	= config.accountCreationParams.jsonMetadata;
 var extensions		= config.accountCreationParams.extensions;
 
 exports.createAccount = function(data){
 
-	var account = data.account;    
+    var account = data.account;    
     var memo 	= { weight_threshold: 1, account_auths: [], key_auths: [[data.memo, 1]] };
     var owner 	= { weight_threshold: 1, account_auths: [], key_auths: [[data.owner, 1]] };
     var active 	= { weight_threshold: 1, account_auths: [], key_auths: [[data.active, 1]] };
